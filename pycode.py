7b399b6edd06399071fba0b233dacd2617d1f4d3
Heman-git07 <heman28102002@gmail.com>
2024-08-30 08:46:24 +0530

    task8

--- pycode.py
+++ pycode.py
@@ -1,18 +1,4 @@
-#We’re trying to figure out how much it rained in the past year! Update the annual_rainfall variable to include the values from September to December.
-january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
-annual_rainfall = january_to_june_rainfall
-
-july_rainfall = 1.05
-annual_rainfall += july_rainfall
-
-august_rainfall = 4.91
-annual_rainfall += august_rainfall
-
-september_rainfall = 5.16
-annual_rainfall = annual_rainfall+september_rainfall
-october_rainfall = 7.20
-annual_rainfall += october_rainfall
-november_rainfall = 5.06
-annual_rainfall += november_rainfall
-december_rainfall = 4.06
-annual_rainfall += december_rainfall
+#Add a comment above the declaration of city_pop with a description of what you think the variable contains.
+city_name = "St. Potatosburg"
+#the city_pop contains number of population
+city_pop = 340000
