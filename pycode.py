894da881afc0bce0c40a6a1d2b1ef8a1080a936c
Heman-git07 <heman28102002@gmail.com>
2024-08-30 09:14:12 +0530

    task12

--- pycode.py
+++ pycode.py
@@ -1,11 +1,7 @@
-#You have come home from the grocery store with 100 cucumbers to split amongst yourself and your 5 roommates (6 people total).
-#Create a variable called whole_cucumbers_per_person that is the integer result of dividing cucumbers by num_people.Print whole_cucumbers_per_person to the console.
-#You realize that the numbers don’t divide evenly and you don’t want to throw out the remaining cucumbers. Create a variable called float_cucumbers_per_person that holds the float result of dividing cucumbers by num_people.Print float_cucumbers_per_person to the console
-cucumbers=100
-num_people=6
-whole_cucumbers_per_person=100/6
-print whole_cucumbers_per_person
-float_cucumbers_per_person=100./6
+#Create a variable called haiku and store this haiku as a multi-line string:
+haiku = """The old pond,
+A frog jumps in:
+Plop!"""
 
 
 
