fb29c283e451de5924edfb2bf59621641c89af05
Heman-git07 <heman28102002@gmail.com>
2024-08-30 11:06:09 +0530

    task16

--- pycode.py
+++ pycode.py
@@ -6,6 +6,9 @@ point_total+=exercises_completed*points_per_exercise
 #The amount of points for each exercise may change, because points don't exist yet
 print "i got",point_total,"points"
 
+# Set the variable brian on line 3!
+brian = "Hello life!"
+
 
 
 
