17a96fdaeb5e69780bcc3250ec9e589adf13620d
Heman-git07 <heman28102002@gmail.com>
2024-08-30 09:41:04 +0530

    task14

--- pycode.py
+++ pycode.py
@@ -1,8 +1,9 @@
-# Hi! I'm Maria and I live in script.py.
-# I'm an expert Python coder.
-# I'm 21 years old and I plan to program cool stuff forever.
-age_is_12=False
-name_is_maria=True
+#Create a variable called product that contains the result of multiplying the float value of float_1 and float_2.
+#Create a string called big_string that says:with the value of product where the X is
+float_1 = 0.25
+float_2 = 40.0
+product=float_1*float_2
+big_string="The product was 10"
 
 
 
