db5f970af02087c50c457ed57b8a576daafa3aac
Heman-git07 <heman28102002@gmail.com>
2024-08-30 11:47:26 +0530

    task21

--- pycode.py
+++ pycode.py
@@ -30,5 +30,10 @@ print fifth_letter
 parrot="Norwegian Blue"
 print len(parrot)
 
+#You can use the lower() method to get rid of all the capitalization in your strings. You call lower() like so:
+parrot = "Norwegian Blue"
+
+print parrot.lower()
+
 
 
