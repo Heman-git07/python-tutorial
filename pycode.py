fbf1ebd1db4fcaa9cf9250532104f0e93370b071
Heman-git07 <heman28102002@gmail.com>
2024-08-30 11:49:25 +0530

    task22

--- pycode.py
+++ pycode.py
@@ -35,5 +35,10 @@ parrot = "Norwegian Blue"
 
 print parrot.lower()
 
+#Call upper() on parrot (after print on line 3) in order to capitalize all the characters in the string!
+parrot = "norwegian blue"
+
+print parrot.upper()
+
 
 
