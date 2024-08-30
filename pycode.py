5dcfa8ce2d2bd592d78a84cacdf41650383df95b
Heman-git07 <heman28102002@gmail.com>
2024-08-30 11:10:27 +0530

    task17

--- pycode.py
+++ pycode.py
@@ -9,6 +9,16 @@ print "i got",point_total,"points"
 # Set the variable brian on line 3!
 brian = "Hello life!"
 
+# Assign your variables below, each on its own line!
+caesar="Graham"
+praline="John"
+viking="Teresa"
+# Put your variables above this line
+
+print caesar
+print praline
+print viking
+
 
 
 
