0538550446e40f3d88cae825935f35a893f06b6a
Heman-git07 <heman28102002@gmail.com>
2024-08-31 09:59:28 +0530

    task31

--- pycode.py
+++ pycode.py
@@ -82,6 +82,14 @@ print "Ah, so your name is %s, your quest is %s, " \
 my_string="heman"
 print len(my_string)
 print my_string.upper()
+#final task
+meal=44.50
+tax=6.75/100
+tip=15.0/100
+meal=meal+meal*tax
+total=meal+meal*tip
+print total
+
 
 
 
