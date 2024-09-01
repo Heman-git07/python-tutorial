7d0a712e993b771e95c224454cfb989058730f8d
Heman-git07 <heman28102002@gmail.com>
2024-09-01 19:40:07 +0530

    task33

--- pycode.py
+++ pycode.py
@@ -94,6 +94,14 @@ from datetime import datetime
 now=datetime.now()
 print now
 
+#print the year,date,time
+from datetime import datetime
+now=datetime.now()
+print now.year
+print now.month
+print now.day
+
+
 
 
 
