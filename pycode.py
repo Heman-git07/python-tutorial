1da9398fcdb9500a1a5fffeb4f3c027665ac394f
default <heman28102002@gmail.com>
2024-09-02 15:00:57 +0000

    task73

--- pycode.py
+++ pycode.py
@@ -627,7 +627,20 @@ duck_index = animals.index("duck")# Use index() to find "duck"
 animals.insert(duck_index,"cobra")
 
 print animals # Observe what prints after the insert operation
+my_list = [1,9,3,8,5,7]
 
+for number in my_list:
+  # Your code here
+  print 2 * number
+  start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+# Your code here!
+for number in start_list:
+  square_list.append(number ** 2)
+square_list.sort()
+
+print square_list
 
 
 
