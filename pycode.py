1ccfdde1e2c399cbd45870197b95371f5eb1ccae
default <heman28102002@gmail.com>
2024-09-02 08:01:24 +0000

    task54

--- pycode.py
+++ pycode.py
@@ -332,6 +332,16 @@ if len(original) > 0 and original.isalpha():
   print original
 else:
   print 'empty'  
+  
+pyg = 'ay'
+original = raw_input('Enter a word:')
+if len(original) > 0 and original.isalpha():
+  word=original.lower()
+  first=original[0]
+  new_word=word+first+pyg
+  print original
+else:
+  print 'empty'
 
 
 
