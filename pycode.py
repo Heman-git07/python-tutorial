9b9791e721478d5708fb04f5e45c78e2a8e93189
default <heman28102002@gmail.com>
2024-09-02 14:24:18 +0000

    task61

--- pycode.py
+++ pycode.py
@@ -401,6 +401,21 @@ print math.sqrt(25)
 # Import *everything* from the math module on line 3!
 
 from math import *
+def biggest_number(*args):
+  print max(args)
+  return max(args)
+    
+def smallest_number(*args):
+  print min(args)
+  return min(args)
+
+def distance_from_zero(arg):
+  print abs(arg)
+  return abs(arg)
+
+biggest_number(-10, -5, 5, 10)
+smallest_number(-10, -5, 5, 10)
+distance_from_zero(-10)
 
 
 
