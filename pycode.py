10fafbcd76d8253f9bbf75ca1f0303ef7fca02d0
default <heman28102002@gmail.com>
2024-09-02 13:11:44 +0000

    task58

--- pycode.py
+++ pycode.py
@@ -379,6 +379,11 @@ def square(n):
 
 square(10)
 
+def power(base, exponent):  # Add your parameters here!
+  result = base ** exponent
+  print "%d to the power of %d is %d." % (base, exponent, result)
+
+power(37, 4)  # Add your arguments here!
 
 
 
