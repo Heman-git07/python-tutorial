67ee03be0fe9380403e54ac195dc22b62c310e6e
default <heman28102002@gmail.com>
2024-09-02 15:20:56 +0000

    task79

--- pycode.py
+++ pycode.py
@@ -766,7 +766,66 @@ for key in prices:
   print key
   print "price: %s" % prices[key]
   print "stock: %s" % stock[key]  
+  
+prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
+
+stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
+
+total = 0
+for food in prices:
+  print prices[food] * stock[food]
+  total = total + prices[food] * stock[food]
+print total
+
+groceries = ["banana", "orange", "apple"]
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
     
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+  total = 0
+  for item in food:
+    total = total + prices[item]
+  return total
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+    
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+  total = 0
+  for item in food:
+    if stock[item] > 0:
+      total += prices[item]
+      stock[item] -= 1
+  return total
+
   
 
 
