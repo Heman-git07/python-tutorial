695cd06797bf439f2944cb0da7e2f9512787d80b
Heman-git07 <heman28102002@gmail.com>
2024-09-02 09:36:23 +0530

    task40

--- pycode.py
+++ pycode.py
@@ -166,6 +166,23 @@ bool_four = True
 # 100 != 10**2
 bool_five = False
 
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 3 < 5  # We already did this one for you!
+
+# Make me false!
+bool_two = 4 > 5
+
+# Make me true!
+bool_three = 2==2
+
+# Make me false!
+bool_four = 5!=5
+
+# Make me true!
+bool_five = 6>=5
+
 
 
 
