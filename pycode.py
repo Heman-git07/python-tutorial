8ae1c7a27b05df061aa4e33be80903b85ee6cd9b
default <heman28102002@gmail.com>
2024-09-02 15:16:47 +0000

    task77

--- pycode.py
+++ pycode.py
@@ -712,6 +712,33 @@ webster = {
 for word in webster:
   print webster[word]
   
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for number in a:
+  if number % 2 == 0:
+    print number
+# Write your function below!
+def fizz_count(x):
+  count = 0
+  for item in x:
+    if item == "fizz":
+      count = count + 1
+  return count
+for letter in "Codecademy":
+  print letter
+    
+# Empty lines to make the output pretty
+print
+print
+
+word = "Programming is fun!"
+
+for letter in word:
+  # Only print out the letter i
+  if letter == "i":
+    print letter
+    
+  
 
 
 
