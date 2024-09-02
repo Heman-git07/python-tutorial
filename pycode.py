572b187a5ff929b82c4e257cbce069c678ba4052
default <heman28102002@gmail.com>
2024-09-02 14:36:33 +0000

    task67

--- pycode.py
+++ pycode.py
@@ -471,7 +471,73 @@ def plane_ride_cost(city):
     return 222
   elif city == "Los Angeles":
     return 475
+  def hotel_cost(nights):
+  return 140 * nights
 
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+def rental_car_cost(days):
+  cost = days * 40
+  if days >= 7:
+    cost -= 50
+  elif days >= 3:
+    cost -= 20
+  return cost
+#We define two simple functions, double(n) and triple(p) that return 2 times or 3 times their input. Notice that they have n and p as their argumentsWe define a third function, add(a, b) that returns the sum of the previous two functions when called with a and b, respectively. Notice that even though the names of the parameters for add(a, b) are different than the names of the parameters for double(n) and triple(p) we can still pass them into those functions as arguments
+def hotel_cost(nights):
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+def rental_car_cost(days):
+  cost = days * 40
+  if days >= 7:
+    cost -= 50
+  elif days >= 3:
+    cost -= 20
+  return cost
+
+def trip_cost(city, days):
+  return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
+def hotel_cost(nights):
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+def rental_car_cost(days):
+  cost = days * 40
+  if days >= 7:
+    cost -= 50
+  elif days >= 3:
+    cost -= 20
+  return cost
+
+def trip_cost(city, days, spending_money):
+  return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money
 
 
 
