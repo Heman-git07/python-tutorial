728ee0926bec44fed9eeb064db661369b040fe7e
default <heman28102002@gmail.com>
2024-09-02 14:54:00 +0000

    task70

--- pycode.py
+++ pycode.py
@@ -563,6 +563,32 @@ def trip_cost(city, days, spending_money):
   return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
 
 print trip_cost("Los Angeles", 5, 600)
+zoo_animals = ["pangolin", "cassowary", "sloth", "dog"];
+# One animal is missing!
+
+if len(zoo_animals) > 3:
+  print "The first animal at the zoo is the " + zoo_animals[0]
+  print "The second animal at the zoo is the " + zoo_animals[1]
+  print "The third animal at the zoo is the " + zoo_animals[2]
+  print "The fourth animal at the zoo is the " + zoo_animals[3]
+numbers = [5, 6, 7, 8]
+
+print "Adding the numbers at indices 0 and 2..."
+print numbers[0] + numbers[2]
+print "Adding the numbers at indices 1 and 3..."
+# Your code here!
+print numbers[1] + numbers[3]
+zoo_animals = ["pangolin", "cassowary", "sloth", "tiger"]
+# Last night our zoo's sloth brutally attacked 
+# the poor tiger and ate it whole.
+
+# The ferocious sloth has been replaced by a friendly hyena.
+zoo_animals[2] = "hyena"
+
+# What shall fill the void left by our dear departed tiger?
+# Your code here!
+zoo_animals[3] = "lion"
+
 
 
 
