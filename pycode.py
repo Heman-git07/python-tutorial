fd3e8eabd19c9c38bbc0f6054265c940baace049
default <heman28102002@gmail.com>
2024-09-03 04:15:37 +0000

    task94

--- pycode.py
+++ pycode.py
@@ -1564,6 +1564,21 @@ class Equilateral(Triangle):
     self.angle1 = self.angle
     self.angle2 = self.angle
     self.angle3 = self.angle
+    
+  #file input/output
+  
+  my_list = [i ** 2 for i in range(1, 11)]
+# Generates a list of squares of the numbers 1 - 10
+
+f = open("output.txt", "w")
+
+for item in my_list:
+  f.write(str(item) + "\n")
+
+f.close()
+
+
+
 
 
     
