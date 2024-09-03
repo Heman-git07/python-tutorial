aca0846fd3ea4d69d8ca52e4c7f718efec300038
default <heman28102002@gmail.com>
2024-09-03 03:35:18 +0000

    task85

--- pycode.py
+++ pycode.py
@@ -1056,3 +1056,26 @@ def print_list(x):
     print x[i]
     
 print_list(n)
+n = [3, 5, 7]
+
+def double_list(x):
+  for i in range(0, len(x)):
+    x[i] = x[i] * 2
+  return x
+# Don't forget to return your new list!
+
+print double_list(n)
+
+def my_function(x):
+  for i in range(0, len(x)):
+    x[i] = x[i]
+  return x
+
+print my_function(range(3)) # Add your range between the parentheses!
+n = [3, 5, 7]
+
+def total(numbers):
+  result = 0
+  for i in range(0,len(numbers)):
+    result += numbers[i]
+  return result
