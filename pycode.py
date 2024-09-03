79b2a22124bf04a76e905e43fa98ca2ccaa9170e
default <heman28102002@gmail.com>
2024-09-03 03:49:54 +0000

    task89

--- pycode.py
+++ pycode.py
@@ -1251,5 +1251,208 @@ else:
   print "You missed my battleship!"
   board[guess_row][guess_col] = "X"
   print_board(board)
+  
+  from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
+
+# Write your code below!
+if guess_row == ship_row and guess_col == ship_col:
+  print "Congratulations! You sank my battleship!"   
+else:
+  if guess_row not in range(5) or \
+    guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+  print_board(board)
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
+
+# Write your code below!
+if guess_row == ship_row and guess_col == ship_col:
+  print "Congratulations! You sank my battleship!"   
+else:
+  if guess_row not in range(5) or \
+    guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+  elif (board[guess_row][guess_col] == 'X'):
+    print "You guessed that one already."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+  print_board(board)
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
+
+if guess_row == ship_row and guess_col == ship_col:
+  print "Congratulations! You sank my battleship!"   
+else:
+  if guess_row not in range(5) or \
+    guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+  elif (board[guess_row][guess_col] == 'X'):
+    print "You guessed that one already."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+  print_board(board)
+from random import randint
+
+board = []
+
+for x in range(5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
+
+if guess_row == ship_row and guess_col == ship_col:
+  print "Congratulations! You sunk my battleship!"
+else:
+  if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+    print "Oops, that's not even in the ocean."
+  elif(board[guess_row][guess_col] == "X"):
+    print "You guessed that one already."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+  # Print (turn + 1) here!
+  print_board(board)
+  
+  from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+# Everything from here on should be in your for loop
+# don't forget to properly indent!
+for turn in range(4):
+  print "Turn", turn + 1
+  guess_row = int(raw_input("Guess Row: "))
+  guess_col = int(raw_input("Guess Col: "))
+
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship!"
+    break
+  else:
+    if guess_row not in range(5) or \
+      guess_col not in range(5):
+      print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == "X":
+      print( "You guessed that one already." )
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+    if (turn == 3):
+      print "Game Over"
+    print_board(board)
 
 
